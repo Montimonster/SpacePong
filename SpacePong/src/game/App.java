b616c13784709f8146599a40c97c11e0f525ddb8
Montimonster <montisanchez2002@gmail.com>
2022-04-26 21:38:12 +0200

    commit

--- SpacePong/src/game/App.java
+++ SpacePong/src/game/App.java
@@ -4,10 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
-
-		System.out.println("Me chupa un huevo");
-		System.out.println("Pues que no te chupe tanto");
+		System.out.println("Hello world");		
 
 	}
 
