8e8d91d6bea4973f9c713f74d494d1004bd0e63a
Montimonster <montisanchez2002@gmail.com>
2022-04-28 23:04:01 +0200

    Creación de la ventana

--- SpacePong/src/game/App.java
+++ SpacePong/src/game/App.java
@@ -1,10 +1,34 @@
 package game;
 
-public class App {
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+
+	public App() {
+
+	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");		
+		JFrame frame = new JFrame("Wallgame");
+		frame.setSize(800, 800);
+		// frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		frame.setUndecorated(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+
+		JFrame frame2 = new JFrame("jFrame 2");
+		frame.add(frame2);
+		frame2.setSize(400, 400);
+		frame2.setBackground(Color.BLUE);
+		// frame2.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		// frame2.setUndecorated(false);
+		frame2.setVisible(true);
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	
 
 		System.out.println("loco");
 		
